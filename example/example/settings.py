eeb14411a4732ddb54b9aee7c563a5ef3818c89e
James Turk <james.p.turk@gmail.com>
2012-12-01 23:06:49 -0500

    initial commit

---
+++ example/example/settings.py
@@ -0,0 +1,63 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'example.db',
+    }
+}
+
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+SECRET_KEY = 'nm!ejfr(-(3nsl+84@m%(m1_wsijwj14855^280qp*-=0g-dv-'
+ROOT_URLCONF = 'example.urls'
+WSGI_APPLICATION = 'example.wsgi.application'
+
+TEMPLATE_DIRS = (
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'social_auth',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'sunlightauth.backends.SunlightBackend',
+)
+
+SUNLIGHT_AUTH_APP_ID = 'a-key'
+SUNLIGHT_AUTH_SECRET = 'a-secret'
+SUNLIGHT_AUTH_SCOPE = []
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ sunlightauth/backends.py
@@ -0,0 +1,53 @@
+""" Sunlight OAuth support.  """
+from urllib import urlencode
+import json
+from social_auth.utils import setting, dsa_urlopen
+from social_auth.backends import BaseOAuth2, OAuthBackend, USERNAME
+
+
+BASE_SUNLIGHT_URL = setting('SUNLIGHT_AUTH_BASE_URL',
+                            'http://login.sunlightfoundation.com/')
+SUNLIGHT_AUTHORIZATION_URL = '%soauth2/authorize/' % BASE_SUNLIGHT_URL
+SUNLIGHT_ACCESS_TOKEN_URL = '%soauth2/token/' % BASE_SUNLIGHT_URL
+SUNLIGHT_USER_DATA_URL = '%sapi/userinfo/' % BASE_SUNLIGHT_URL
+
+
+class SunlightBackend(OAuthBackend):
+    """Sunlight OAuth authentication backend"""
+    name = 'sunlight'
+
+    # use this key as a globally unique user id
+    ID_KEY = 'email'
+
+    def get_user_details(self, response):
+        """Return user details from Sunlight login server"""
+        return {'username': response.get('username'),
+                'email': response.get('email'),
+                'first_name': response.get('first_name'),
+                'last_name': response.get('last_name'),
+               }
+
+
+class SunlightAuth(BaseOAuth2):
+    """Sunlight OAuth2 mechanism"""
+    AUTHORIZATION_URL = SUNLIGHT_AUTHORIZATION_URL
+    ACCESS_TOKEN_URL = SUNLIGHT_ACCESS_TOKEN_URL
+    AUTH_BACKEND = SunlightBackend
+    SETTINGS_KEY_NAME = 'SUNLIGHT_AUTH_APP_ID'
+    SETTINGS_SECRET_NAME = 'SUNLIGHT_AUTH_SECRET'
+    SCOPE_VAR_NAME = 'SUNLIGHT_AUTH_SCOPE'
+    SCOPE_SEPARATOR = ','
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        url = SUNLIGHT_USER_DATA_URL + '?' + urlencode({
+            'access_token': access_token
+        })
+        try:
+            return json.load(dsa_urlopen(url))
+        except ValueError:
+            return None
+
+
+# expose backend to django-social-auth
+BACKENDS = {'sunlight': SunlightAuth}
