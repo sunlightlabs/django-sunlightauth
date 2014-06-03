a8ec484802b24fcaa53b7cdfadf07594f0305c39
Paul Tagliamonte <tag@pault.ag>
2014-06-03 19:13:19 -0400

    Guard the settings with a hasattr to default to the central login server.

--- sunlightauth/views.py
+++ sunlightauth/views.py
@@ -22,6 +22,10 @@ def logout(request):
     ## prepend host (use sites framework?)
     redirect_to = 'http://' + Site.objects.get_current().domain + redirect_to
 
+    BASE = 'https://login.sunlightfoundation.com/'
+    if hasattr(settings, 'SUNLIGHT_AUTH_BASE_URL'):
+        BASE = settings.SUNLIGHT_AUTH_BASE_URL
+
     logout_url = ''.join((settings.SUNLIGHT_AUTH_BASE_URL,
                           'accounts/logout/?next=',
                           redirect_to))
